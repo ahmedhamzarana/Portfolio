6b43c06b10162149dc1b2ec7034283e067d06906
Hamza Rana <ahmedhamzarana2244@gmail.com>
2024-08-17 16:57:06 +0500

    I will create a random Portfolio

---
+++ main.js
@@ -0,0 +1,16 @@
+//var typed = new Typed('.text',{
+//    Strings:["Frontend Developer", "Web Developer", "Web Desingner"],
+//    typeSpeed:100,
+//    backSpeed:100,
+//    backDelay:1000,
+//    loop:true
+//});
+const typed = new Typed('.text',{
+    strings: ['Frontend Developer', "Web Developer", 'Freelancer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+
+});
+    
